d18cc5d4d02df8255c5c1b3ee13d95a21a8d6e65
Joel Kaardal <jkaardal@gmail.com>
2020-07-25 11:31:33 -0700

    add install script

--- __init__.py
+++ __init__.py
@@ -1,1 +1,3 @@
+# In case someone want to run this from the git repo, this avoids csvnav.csvnav. If installed in dist-packages, this
+# file is ignored.
 from .csvnav import Navigator
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+# Install csvnav.py in dist-packages.
+setup(
+    name='csvnav',
+    version='0.1',
+    py_modules=['csvnav'],
+    author='Joel Kaardal',
+    url='https://github.com/jkaardal/csvnav',
+)
