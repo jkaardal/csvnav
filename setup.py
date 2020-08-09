69399de084a3a2effc569bbc73d7a2d80a7a2895
Joel Kaardal <jkaardal@gmail.com>
2020-08-09 10:58:48 -0700

    switch to setup tools, add args

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
 
-# Install csvnav.py in dist-packages.
-setup(
-    name='csvnav',
-    version='0.1',
-    py_modules=['csvnav'],
-    author='Joel Kaardal',
-    url='https://github.com/jkaardal/csvnav',
-)
+setup_args = {
+    'name': 'csvnav',
+    'version': '0.1',
+    'description': 'A python class for memory-efficient navigation of CSV/Text files.',
+    'long_description_content_type': 'text/markdown',
+    'long_description': open('README.md', 'r').read(),
+    'license': 'MIT',
+    'py_modules': ['csvnav'],
+    'author': 'Joel Kaardal',
+    'author_email': 'jkaardal@gmail.com',
+    'keywords': ['data-science', 'csv', 'machine-learning', 'data-analysis', 'memory-management'],
+    'url': 'https://github.com/jkaardal/csvnav',
+    'download_url': 'https://pypi.org/project/csvnav/',
+}
+
+if __name__ == '__main__':
+    setup(**setup_args)
