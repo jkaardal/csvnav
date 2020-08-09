071d162ef18a4b24489feea1bb5c78686dd0a5a9
Joel Kaardal <jkaardal@gmail.com>
2020-08-09 11:53:48 -0700

    readme fixes

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
+with open('README.md', 'r') as fp:
+    readme = fp.read()
+readme = readme[:readme.index('## About')]
+
 setup_args = {
     'name': 'csvnav',
     'version': '0.1.0',
     'description': 'A python 3 class for memory-efficient navigation of CSV/Text files.',
     'long_description_content_type': 'text/markdown',
-    'long_description': open('README.md', 'r').read(),
+    'long_description': readme,
     'license': 'MIT',
     'py_modules': ['csvnav'],
     'author': 'Joel Kaardal',
